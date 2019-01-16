b64b80ed3b4d41a52ac7b65a2a06236068cff9cf
= <rbartolome@sanaval.com>
2019-01-16 11:44:50 +0100

    Clase Fin Clase 9 - admin LTE

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layout')
+
+@section('contenido')
+	<h1>Dash</h1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 Route::get('posts', 'PostController@show')->name('show');
 
 Route::get('/', function () {
 	return 'home, ruta inicio en routes/web';
     //return view('welcome');
+});
+Route::get('admin', function(){
+	return view('admin.dashboard');
 });
